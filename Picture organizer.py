556f392e3e1c04fc85a4f48a499d16329e3111ce
JoeC <41484989+JoeCofone@users.noreply.github.com>
2018-10-09 23:22:02 -0400

    Dictionaries not Files...need to keepsake Pics
    Should always alias "Pictures List.csv" and make it read only.

--- Picture organizer.py
+++ Picture organizer.py
@@ -1,10 +1,11 @@
 #Plan:
 #BACKUP THE FILES!!!
-# 1: Read files,
-# 2: get timestamps,
-# 3: move to "yyyy-mm(Mon)" folder
+# -1: Read files,
+# -2: get timestamps,
+     #compare to previous imports
+# -3: move to "yyyy-mm(Mon)" folder
+     #document import
 # 4: files sorted and labeled by day and time
-# 5: add lookup text for 
 from PIL import Image
 from PIL.ExifTags import TAGS
 import os
@@ -13,7 +14,7 @@ import calendar
 import shutil
 import csv
 today = datetime.datetime.today().strftime('%Y-%m-%d')
-srcdir = 'C:/Users/joeco/Pictures/To Import 2018-08-02'#look this up
+srcdir = 'C:/Users/joeco/Pictures/To Import 2018-10-08'#look this up
 destdir = 'C:/Users/joeco/Pictures'
 srcfldrs = os.listdir(srcdir)#srcfldrs
 fldrs = os.listdir(destdir)#fldrs
@@ -88,7 +89,6 @@ def get_model(all_attrs):
      except:
           model = 'Anonymous'
      return model
-count = 0
 #The process should be:
 #-Loop through the source folders and get pic info
 #-Compare current import list to current folder list
@@ -104,61 +104,35 @@ complist['Pic'] = []
 complist['Fldr'] = []
 complist['Path'] = []
 compcount = 0
-for fldr in srcfldrs:#forTS: fldr = '2018-05'
-     print(fldr)
-     if fldr.rfind('.') < 0:
-          pics = os.listdir(srcdir + '/' + fldr)
-          for pic in pics:#forTS: pic = 'IMG_9688.JPG'
-               if pic.upper().rfind('.JPG') >= 0:
-                    fname = srcdir + '/' + fldr + '/' + pic
-                    all_attrs = get_exif_data(fname)
-                    #first figure when the pic was taken/created
-                    fldr_name, fday, fhms = get_datetime(all_attrs)
-                    #now get the GPS info
-                    gpsi = get_gpsinfo(all_attrs)
-                    #now get the Model
-                    model = get_model(all_attrs)
-                    #finfo == file information
-                    #??Needed??: finfo = fname + ', ' + model + ', ' + destdir + '/' + fldr_name + ', ' + fday + ', ' + fhms + ', ' + str(gpsi['Lat']) + ', ' + str(gpsi['Lon']) + '\n'
-                    #ofinfo == original file information
-                    ofinfo = '"' + model + '", ' + fday + ', "' + fhms + '", ' + str(gpsi['Lat']) + ', ' + str(gpsi['Lon']) + ', "' + pic + '", "' + fldr_name + '", "' + fname + '"\n'
-                    complist['Model'].append(model)
-                    complist['Date'].append(fday)
-                    complist['Time'].append(fhms)
-                    complist['Lat'].append(gpsi['Lat'])
-                    complist['Lon'].append(gpsi['Lon'])
-                    complist['Pic'].append(pic)
-                    complist['Fldr'].append(fldr_name)
-                    complist['Path'].append(fname)
-                    #picinfo = open(destdir + '/' + 'Comparison List.csv', 'a')
-                    #picinfo.write(ofinfo)
-                    #picinfo.close()
-                    compcount = compcount + 1
-#This process will compare the "Comparison List" (from the folder inventoried for download)
-#to the "Picture List" and see what files haven't been downloaded already.
-#Those will be added to the "Import List".
-try:
-     imported = open(destdir + '/' + 'Picture List.csv', 'r')
-     files_imported = imported.readlines()
-     to_import = open(destdir + '/' + 'Comparison List.csv', 'r')
-     files_to_import = to_import.readlines()
-     with open(destdir + '/' + 'Import List.csv', 'w') as import_list:
-         for line in files_to_import:
-             if line not in files_imported:
-                 import_list.write(line)
-     imported.close()
-     to_import.close()
-     import_list.close()
-except:
-     try:
-          open(destdir + '/' + 'Picture List.csv', 'r').close()
-     except:
-          open(destdir + '/' + 'Comparison List.csv', 'r').close()
-     comp_list = destdir + '/' + 'Comparison List.csv'
-     imp_list = destdir + '/' + 'Import List.csv'
-     os.rename(comp_list, imp_list)
+pics = os.listdir(srcdir)
+for pic in pics:#forTS: pic = 'IMG_9688.JPG'
+     if pic.upper().rfind('.JPG') >= 0:
+          fname = srcdir + '/' + pic
+          all_attrs = get_exif_data(fname)
+          #first figure when the pic was taken/created
+          fldr_name, fday, fhms = get_datetime(all_attrs)
+          #now get the GPS info
+          gpsi = get_gpsinfo(all_attrs)
+          #now get the Model
+          model = get_model(all_attrs)
+          #finfo == file information
+          #??Needed??: finfo = fname + ', ' + model + ', ' + destdir + '/' + fldr_name + ', ' + fday + ', ' + fhms + ', ' + str(gpsi['Lat']) + ', ' + str(gpsi['Lon']) + '\n'
+          #ofinfo == original file information
+          ofinfo = '"' + model + '", ' + fday + ', "' + fhms + '", ' + str(gpsi['Lat']) + ', ' + str(gpsi['Lon']) + ', "' + pic + '", "' + fldr_name + '", "' + fname + '"\n'
+          complist['Model'].append(model)
+          complist['Date'].append(fday)
+          complist['Time'].append(fhms)
+          complist['Lat'].append(gpsi['Lat'])
+          complist['Lon'].append(gpsi['Lon'])
+          complist['Pic'].append(pic)
+          complist['Fldr'].append(fldr_name)
+          complist['Path'].append(fname)
+          #picinfo = open(destdir + '/' + 'Comparison List.csv', 'a')
+          #picinfo.write(ofinfo)
+          #picinfo.close()
+          compcount = compcount + 1
 #this is the thorough and methodical way to import:
-try:
+try:#load pics into a dictionary and go down the rabbit hole to see if any match
      piclist = {}
      piclist['Model'] = []
      piclist['Date'] = []
@@ -172,67 +146,85 @@ try:
      pic_csv = csv.DictReader(open(destdir + '/' + 'Picture List.csv', 'r'), fieldnames = ['Model', 'Date', 'Time', 'Lat', 'Lon', 'Pic', 'Fldr', 'Path'], delimiter = ',', quotechar = '"')
      for row in pic_csv:
           for key in row:
-               if key == 'Date' or key == 'Pic' or key == 'Path' or key == 'Fldr':
+               if key == 'Lat' or key == 'Lon':
+                    piclist[key].append(float(row[key].strip().replace('"', '')))
+               else:
                     piclist[key].append(row[key].strip().replace('"', ''))
-               else:     
-                    piclist[key].append(row[key])
           piccount = piccount + 1
+     implist = {}
+     implist['Model'] = []
+     implist['Date'] = []
+     implist['Time'] = []
+     implist['Lat'] = []
+     implist['Lon'] = []
+     implist['Pic'] = []
+     implist['Fldr'] = []
+     implist['Path'] = []
+     comprec = 0
+     impcount = 0
+     while comprec < compcount:#comprec = 1953
+          keep = 1
+          picrec = 0
+          while picrec < piccount:#picrec = 4437
+               if complist['Pic'][comprec] == 'IMG_9514.JPG' and piclist['Pic'][picrec] == 'IMG_9514.JPG':
+                    print(str(comprec), str(picrec))
+                    print(complist['Model'][comprec] + 'and' + piclist['Model'][picrec])
+                    print(complist['Model'][comprec] == piclist['Model'][picrec])
+                    print(complist['Date'][comprec] + 'and' + piclist['Date'][picrec])
+                    print(complist['Date'][comprec] == piclist['Date'][picrec])
+                    print(complist['Time'][comprec] + 'and' + piclist['Time'][picrec])
+                    print(complist['Time'][comprec] == piclist['Time'][picrec])
+                    print(round(complist['Lat'][comprec],6) == round(piclist['Lat'][picrec],6))
+                    print(round(complist['Lon'][comprec],6) == round(piclist['Lon'][picrec],6))
+               if complist['Model'][comprec] == piclist['Model'][picrec]:
+                    if complist['Date'][comprec] == piclist['Date'][picrec]:
+                         if complist['Time'][comprec] == piclist['Time'][picrec]:
+                              if round(complist['Lat'][comprec],6) == round(piclist['Lat'][picrec],6):
+                                   if round(complist['Lon'][comprec],6) == round(piclist['Lon'][picrec],6):
+                                        keep = 0#if all the above are the same, then it's the same picture
+               picrec = picrec + 1
+          if keep == 1:
+               implist['Model'].append(complist['Model'][comprec])
+               implist['Date'].append(complist['Date'][comprec])
+               implist['Time'].append(complist['Time'][comprec])
+               implist['Lat'].append(complist['Lat'][comprec])
+               implist['Lon'].append(complist['Lon'][comprec])
+               implist['Pic'].append(complist['Pic'][comprec])
+               implist['Fldr'].append(complist['Fldr'][comprec])
+               implist['Path'].append(complist['Path'][comprec])
+               impcount = impcount + 1
+          #print(str(comprec))
+          comprec = comprec + 1
 except:#if the picture file is empty...which should only be once!!!
      implist = complist
      impcount = compcount
-#action items below...to be handled once final import list is vetted
-#Need to go row-by-row in the import file, parse the row, and move and rename the file
-implist = {}
-implist['Model'] = []
-implist['Date'] = []
-implist['Time'] = []
-implist['Lat'] = []
-implist['Lon'] = []
-implist['Pic'] = []
-implist['Fldr'] = []
-implist['Path'] = []
-count = 0
-# Make sure list of keys is consistent in the line below
-csv_file = csv.DictReader(open(destdir + '/' + 'Import List.csv', 'r'), fieldnames = ['Model', 'Date', 'Time', 'Lat', 'Lon', 'Pic', 'Fldr', 'Path'], delimiter = ',', quotechar = '"')
-for row in csv_file:
-     for key in row:
-          if key == 'Date' or key == 'Pic' or key == 'Path' or key == 'Fldr':
-               flist[key].append(row[key].strip().replace('"', ''))
-          else:     
-               flist[key].append(row[key])
-          if count == 0:
-               print(key + ' is ' + row[key])
-     count = count + 1
-print(count)
+     print("Something's wrong")
 #this will give the "count" number just enough zero-padding
-zfmt = '0' + str(len(str(count))) + 'd'
+zfmt = '0' + str(len(str(impcount))) + 'd'
 print(zfmt)
-#print(flist['Date'][6041:6150])
-#print(flist['Path'][6041])
-#print(flist['Pic'][6041:6142])
-#print(destdir + '/' + flist['Pic'][6041])
+#this will loop through the implist, copy the files, and append to the "Pictures List.csv"
 pic2imp = 0
-while pic2imp < count:
+while pic2imp < impcount:
      imported = open(destdir + '/' + 'Picture List.csv', 'a')
-     for key in flist.keys():
+     for key in implist.keys():
           #this first part will handle the folder creation
           if key == 'Fldr':
                #if the fldr_name is already spoken for, don't add it ot the list of ones to be created
                fldr_presence = 0
                for new_fldr in fldrs:
-                    if flist['Fldr'][pic2imp] == new_fldr:
+                    if implist['Fldr'][pic2imp] == new_fldr:
                          fldr_presence = 1
                if fldr_presence == 0:
-                    nxtfldr = flist['Fldr'][pic2imp]
+                    nxtfldr = implist['Fldr'][pic2imp]
                     fldrs.append(nxtfldr)
-                    os.mkdir(srcdir + '/' + flist['Fldr'][pic2imp])
+                    os.mkdir(destdir + '/' + implist['Fldr'][pic2imp])
           #this second part will handle the file import and printing to the "Pictures List".
           if key == 'Path':
                #The easy part is copying...the hard part is naming
-               destfname = destdir + '/' + flist['Fldr'][pic2imp] + '/' + today + '.' + f"{pic2imp:04d}" + '.jpg'
+               destfname = destdir + '/' + implist['Fldr'][pic2imp] + '/' + today + '.' + f"{pic2imp:04d}" + '.jpg'
                #print(destfname)
-               #shutil.copy2(flist[key][pic2imp], destfname)
-               nfinfo = '"' + flist['Model'][pic2imp] + '", ' + flist['Date'][pic2imp] + ', "' + flist['Time'][pic2imp] + '", ' + str(flist['Lat'][pic2imp]) + ', ' + str(flist['Lon'][pic2imp]) + ', "' + flist['Pic'][pic2imp] + '", "' + flist['Fldr'][pic2imp] + '", "' + destfname + '"\n'
+               shutil.copy2(implist[key][pic2imp], destfname)
+               nfinfo = '"' + implist['Model'][pic2imp] + '", ' + implist['Date'][pic2imp] + ', "' + implist['Time'][pic2imp] + '", ' + str(implist['Lat'][pic2imp]) + ', ' + str(implist['Lon'][pic2imp]) + ', "' + implist['Pic'][pic2imp] + '", "' + implist['Fldr'][pic2imp] + '", "' + destfname + '"\n'
                imported.write(nfinfo)
      pic2imp = pic2imp + 1
      print(pic2imp)
